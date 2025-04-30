f0f747ddd8d48a1403d3b81a86b5aa26f6cace65
Vitor Marques <vitormarquesdev@gmail.com>
2025-04-30 17:42:37 -0300

    docs: add comprehensive README and LICENSE files

--- obj/Debug/net8.0/ECommerce.AssemblyInfo.cs
+++ obj/Debug/net8.0/ECommerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7d5254170205a9a95cf38fcdf66b423a1277ae1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f6643c3f0bc07a8a4345cd25909248b92c574b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
